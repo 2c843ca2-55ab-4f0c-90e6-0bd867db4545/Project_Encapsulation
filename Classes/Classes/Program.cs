f7cdc5db253282b2a4d99509406181a312384f7b
2c843ca2-55ab-4f0c-90e6-0bd867db4545 <chenakeshava1@gmail.com>
2016-04-16 21:31:49 +0530

    Initial Commit

---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Classes
+{
+    public class A
+    {
+        public virtual void show()
+        {
+            Console.WriteLine("A");
+        }
+    }
+
+    public class B : A
+    {
+        public override void show()
+        {
+            Console.WriteLine("B");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A ObjA1 = new A();
+            //ObjA.show();
+
+            B ObjB = new B();
+            //ObjB.show();
+            //object p = ObjA1;
+            object p = ObjB;
+
+            A Obja = (A)p;
+            Obja.show();
+
+
+
+            //ObjB = Obja;
+            //ObjB.show();
+
+
+            //B Objb = new A();
+            //Objb.show();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
